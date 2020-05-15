d69f4dfb6bc22b98a1fe82cd5de8ecf1cdb13ae9
Payton H <paytonbh@yahoo.com>
2020-05-14 23:13:36 -0400

    loads a random youtube video on the homepage using the api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 import { youtubeApiKey } from './secureKeys';
 
-let db = admin.firestore();
+const db = admin.firestore();
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   const message = "Let the noise become!";
@@ -17,13 +17,13 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 export const createnoise = functions.https.onRequest((request, response) => {
   if (request.method === "POST") {
     try {
-      let artist = request.body.artist || 'John Cale';
-      let album = request.body.album || 'The Academy in Peril';
-      let genre = request.body.genre || 'Rock';
-      let rating = request.body.rating || 6;
-      let randomNumber = getRandomInt(50000);
+      const artist = request.body.artist || 'John Cale';
+      const album = request.body.album || 'The Academy in Peril';
+      const genre = request.body.genre || 'Rock';
+      const rating = request.body.rating || 6;
+      const randomNumber = getRandomInt(50000);
   
-      let data = {
+      const data = {
         artist: artist,
         album: album,
         genre: genre,
@@ -47,7 +47,9 @@ export const createnoise = functions.https.onRequest((request, response) => {
       response.send("Something went wrong.");
     }
   } else {
-    let newalbums = db.collection('newalbums');
+    response.set('Access-Control-Allow-Origin', '*');
+
+    const newalbums = db.collection('newalbums');
     newalbums.where('randomNumber', '<=', getRandomInt(50000)).orderBy('randomNumber', 'desc').limit(1).get()
     .then((snapshot: any) => {
 
@@ -61,7 +63,9 @@ export const createnoise = functions.https.onRequest((request, response) => {
 
       const promiseData = getYoutubeVideoForKeyword(queryString);
       promiseData.then(data => {
-        const videoLink = `https://www.youtube.com/watch?v=${data.data.items[0].id.videoId}`;
+        console.log(JSON.stringify(data.data.items, undefined, 2));
+
+        const videoLink = `${data.data.items[0].id.videoId}`;
         response.statusCode = 200;
         response.send(videoLink);
       })
@@ -77,7 +81,7 @@ export const createnoise = functions.https.onRequest((request, response) => {
 const getYoutubeVideoForKeyword = async (keyword: string) => {
   console.log(encodeURIComponent(keyword));
 
-  return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(keyword)}&key=${youtubeApiKey}`);
+  return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(keyword)}&key=${youtubeApiKey}&type=video`);
 };
 
 function getRandomInt(max: number) {
