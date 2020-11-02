3648c2ccbb298457255c9c0d67eb8b0be16ae6c0
Payton H <paytonbh@yahoo.com>
2020-11-01 23:11:39 -0500

    adds video playing

--- src/App.js
+++ src/App.js
@@ -1,46 +1,56 @@
 import React, { Component } from 'react';
 import DiamondButton from './components/DiamondButton';
+import ReactPlayer from 'react-player'
 
 export default class App extends Component {
   state = {
-    videoPlaying: null, // becomes a youtube link
+    videoPlaying: false,
+    videoToPlay: null,
     gradient1: 0xEE7171,
     gradient2: 0x7176EE,
+    errorText: '',
   };
 
   constructor(props) {
     super(props);
   }
 
-  // updateGradient = () => {
-  //   this.setState({ gradient1: this.state.gradient1 - 10, gradient2: this.state.gradient2 + 10 })
-
-  //   setTimeout(() => {
-  //     this.updateGradient()
-  //   }, 100)
-  // }
-
-  // componentDidMount() {
-  //   this.updateGradient();
-  // }
+  getVideo = () => {
+    this.setState({ videoPlaying: true });
+    
+    fetch('https://us-central1-unheardnoise.cloudfunctions.net/createnoise')
+    .then(response => response.text())
+    .then(data => {
+      this.setState({ videoToPlay: data });
+    })
+    .catch(error => {
+      console.log('Something went wrong. Please try again later.');
+      this.setState({ errorText: 'Something went wrong. Please try again later.' });
+    })
+  }
 
   render() {
-    //console.log(`linear-gradient(139.46deg, #${this.state.gradient1.toString(16)} 18.55%, #${this.state.gradient2.toString(16)} 100%)`);
-
     return (
       <div style={styles.body}>
-        <div style={this.state.videoPlaying ? // background
-        //backgroundImage: `linear-gradient(139.46deg, #EE7171 18.55%, #7176EE 100%)`, 
-        //backgroundImage: `linear-gradient(139.46deg, #${this.state.gradient1.toString(16)} 18.55%, #${this.state.gradient2.toString(16)} 100%)`,
-        { ...styles.background,
-          opacity: 1.0,
-        } : 
-          styles.background}/>
+        <div style={this.state.videoPlaying ? { ...styles.background, opacity: 1.0 } : styles.background} />
 
-        <p className="blackBackground" style={styles.title}>
+        <p style={this.state.videoPlaying ? { ...styles.title, color: '#71EEEE' } : styles.title}>
           UNHEARD NOISE
         </p>
-        <DiamondButton onClick={() => this.setState({ videoPlaying: 'some link...' })}>
+
+        {this.state.videoPlaying && 
+          <ReactPlayer
+            url={this.state.videoToPlay ? `https://youtube.com/watch?v=${this.state.videoToPlay}` : null}
+            playing={true}
+            style={{ margin: 50, marginBottom: 100 }}
+            controls={true}
+          />
+        }
+
+        {this.state.errorText && 
+          <p style={styles.error}>{this.state.errorText}</p>
+        }
+        <DiamondButton onClick={() => this.getVideo()}>
           {this.state.videoPlaying ? 'Discover More' : 'Discover'}
         </DiamondButton>
       </div>
@@ -57,8 +67,17 @@ let styles = {
     textAlign: 'center',
     fontFamily: 'Montserrat',
     fontSize: '48px',
-    transition: '.3s',
     letterSpacing: '14px',
+    transition: '5s',
+  },
+  error: {
+    color: 'black',
+    borderRadius: '5px',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    fontFamily: 'Montserrat',
+    fontSize: '16px',
   },
   body: {
     width: '100%',
@@ -68,6 +87,7 @@ let styles = {
     display: 'flex',
     flexDirection: 'column',
     backgroundColor: 'rgba(0, 0, 0, 0)',
+    paddingVertical: 100,
   },
   background: {
     width: '100%',
